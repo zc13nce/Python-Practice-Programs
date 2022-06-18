e9c515494a4e9fbe27d57722dea85554318e8da7
zc13nce <95826978+zc13nce@users.noreply.github.com>
2022-06-18 11:01:59 -0500

    Create split_array_move_end.py

---
+++ split_array_move_end.py
@@ -0,0 +1,38 @@
+# Python program to split array and move first
+# part to end.
+
+def splitArr(arr, n, k):
+    for i in range(0, k):
+        x = arr[0]
+        for j in range(0, n-1):
+            arr[j] = arr[j + 1]
+
+        arr[n-1] = x
+
+# main
+arr = [12, 10, 5, 6, 52, 36]
+n = len(arr)
+position = 2
+
+splitArr(arr, n, position)
+
+for i in range(0, n):
+    print(arr[i], end = ' ')
+
+# Code contributed by Mohit Gupta_OMG <0_o)>
+# Mythial creature^^^^
+
+# Python program to split array and move first
+# part to end.
+
+def splitArr(a, n, k):
+    b = a[:k]
+    return (a[k::]+b[::])
+
+# main
+arr = [12, 10, 5, 6, 52, 36]
+n = len(arr)
+position = 2
+arr = splitArr(arr, n , position)
+for i in range(0, n):
+    print(arr[i], end = ' ')
