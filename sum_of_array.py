0853e4a6e7361e2be350fa8f851c87285bed9415
zc13nce <95826978+zc13nce@users.noreply.github.com>
2022-06-18 11:08:06 -0500

    Create sum_of_array.py

---
+++ sum_of_array.py
@@ -0,0 +1,50 @@
+# Python 3 code to find sum
+# of elements in given array
+def _sum(arr):
+
+    # initialize a variable
+    # to store the sum
+    # while iterating through
+    # the array later
+    sum = 0
+
+    # iterate through the array 
+    # and add each element to the sum variable
+    # one at a time
+    for i in arr:
+        sum = sum + i
+
+    return(sum)
+
+# driver function
+arr = []
+# input values to list
+arr = [12, 3, 4, 15]
+
+# calculating length of array
+n = len(arr)
+
+ans = _sum(arr)
+
+# display sum
+print ('Sum of the array is ', ans)
+
+# This code is contributed by Humanshu Ranjan
+
+# Python 3 code to find sum
+# of elements in given array
+# driver function
+arr = []
+
+# input values to list
+arr = [12, 3, 4, 15]
+
+# sum() is an inbuilt function in python that adds
+# all the elemtns in list, set and tuples and returns
+# the value
+ans = sum(arr)
+
+# display sum
+print('Sum of the array is ',ans)
+
+# This code is contributed by Dhananjay Patil
