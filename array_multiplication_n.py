d5acd807924924207c5d8262a236d26e975a6875
zc13nce <95826978+zc13nce@users.noreply.github.com>
2022-06-18 11:01:07 -0500

    Create array_multiplication_n.py

---
+++ array_multiplication_n.py
@@ -0,0 +1,8 @@
+from functools import reduce
+def find_remainder(arr,n):
+    sum_1 = reduce(lambda x,y:x*y,arr)
+    remainder = sum_1%n
+    print(remainder)
+    arr = [100, 10, 5, 25, 35, 14]
+    n = 11
+    find_remainder(arr,n)
